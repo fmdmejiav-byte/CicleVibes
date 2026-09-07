05512e8ad4e214d208ff08065035d8c5b65bbd4b
Fabian Duran <fmdmejia@gmail.com>
2026-09-06 19:33:44 -0500

    actualizacion 0.0.3

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -2,15 +2,11 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ResetPasswordNotification extends Notification implements ShouldQueue
+class ResetPasswordNotification extends Notification
 {
-    use Queueable;
-
     /**
      * El token de restablecimiento generado por el broker de Laravel.
      */
